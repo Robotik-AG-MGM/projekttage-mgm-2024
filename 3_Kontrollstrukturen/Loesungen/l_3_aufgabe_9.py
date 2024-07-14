5c27d23244808988695a2b86acbbed4e2d6c7afe
Ludwig Ettner <ludwig.ettner@web.de>
2024-07-14 22:07:49 +0200

    chore: Refactor code for better readability and maintainability

--- 3_Kontrollstrukturen/Loesungen/l_3_aufgabe_9.py
+++ 3_Kontrollstrukturen/Loesungen/l_3_aufgabe_9.py
@@ -2,11 +2,11 @@ note = 85
 
 if note >= 90:
     print("Sehr gut!")
-elif note >= 80 and note <= 89:
+elif 80 <= note <= 89:
     print("Gut!")
-elif note >= 70 and note <= 79:
+elif 70 <= note <= 79:
     print("Befriedigend")
-elif note >= 60 and note <= 69:
+elif 60 <= note <= 69:
     print("Ausreichend")
 else:
     print("Durchgefallen")
